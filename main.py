1cad0f0118234910af3cdf5f23652c7233451dcb
AdrienPalomo <77984830+AdrienPalomo@users.noreply.github.com>
2021-01-25 10:29:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Shaq rpg")
+import turtle as trtl
+import random
+
+wn = trtl.Screen()
+wn.setup(width=1.0, height=1.0)
+
+drawer = trtl.Turtle()
+drawer.pensize(5)
+
+def make_home():
+    drawer.penup()
+    drawer.goto(-600,-200)
+    drawer.pendown()
+    drawer.goto(600,-200)
+    drawer.goto(600,-400)
+    drawer.goto(-600,-400)
+    drawer.goto(-600,-200)
+    drawer.penup
+
+make_home()
+
+
+
+
+
+
+wn.listen()
+wn.mainloop()
