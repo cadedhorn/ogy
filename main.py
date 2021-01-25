3a56ee30061b17cdd1c6cc9f159ffae2cfa8b862
AdrienPalomo <77984830+AdrienPalomo@users.noreply.github.com>
2021-01-25 10:51:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,41 @@ wn.setup(width=1.0, height=1.0)
 
 drawer = trtl.Turtle()
 drawer.pensize(5)
+drawer.speed(0)
 
 def make_home():
-    drawer.penup()
-    drawer.goto(-600,-200)
+    drawer.pu()
+    drawer.goto(-700,-200)
     drawer.pendown()
-    drawer.goto(600,-200)
-    drawer.goto(600,-400)
-    drawer.goto(-600,-400)
-    drawer.goto(-600,-200)
-    drawer.penup
+    drawer.goto(700,-200)
+    drawer.goto(700,-400)
+    drawer.goto(-700,-400)
+    drawer.goto(-700,-200)
+    drawer.goto(0,-200)
+    drawer.goto(0,-400)
+    drawer.goto(-350,-400)
+    drawer.goto(-350,-200)
+    drawer.goto(350,-200)
+    drawer.goto(350,-400)
+    y = 0
+    for i in range(3):
+        drawer.pu()
+        drawer.goto(-700,(200-y))
+        drawer.pendown()
+        drawer.goto(-300,(200-y))
+        drawer.goto(-300,(150-y))
+        drawer.goto(-700,(150-y))
+        drawer.goto(-700,(200-y))
+        y = y + 100
+    drawer.pu()
+    drawer.goto(-300,-100)
+    drawer.pendown()
+    drawer.goto(700,-100)
+    drawer.goto(700,-150)
+    drawer.goto(-300,-150)
+    drawer.goto(-300,-100)
 
 make_home()
-
-
-
-
-
 
 wn.listen()
 wn.mainloop()
