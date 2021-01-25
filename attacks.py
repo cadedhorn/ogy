37ba1439822cd80b35e9c51b2a80f951739d67ef
Cade Horn <cadedhorn@gmail.com>
2021-01-25 13:49:49 -0700

    game

--- attacks.py
+++ attacks.py
@@ -6,9 +6,9 @@ shaq_hp = 200
 beet_attacks = ["nae nae", "whip", "flex", "heal"]
 i_beet = random.randint(0,3)
 j = beet_attacks[i_beet]
-if j == "nae nae":
+if j in beet_attacks:
     shaq_hp -= 50
     print("Shaq took 50 damage!")
 
 
-print(attacks[i])
+print(beet_attacks[i_beet])
