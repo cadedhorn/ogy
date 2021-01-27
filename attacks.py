05db62edd9bad1e02134a9bd1f5e8d7c217fd7e0
Cade Horn <cadedhorn@gmail.com>
2021-01-26 18:41:25 -0700

    ogy

--- attacks.py
+++ attacks.py
@@ -1,6 +1,6 @@
 import turtle as trtl
 import random
-import main
+import render
 #Stats declaration (in the future read a stats.txt)
 shaq_hp = 200
 shaq_speed = 100
@@ -16,25 +16,25 @@ shaq_choice = 0
 #functions
 def shaqchoose():
     global shaq_choice, shaq_attacks, shaq_hp, shaq_speed, shaq_power, beet_hp, beet_attacks, beet_speed, beet_power
-    main.type_fight("WTF ATTACK YOU WANNA DO HOMIE: ")
+    render.type_fight("WTF ATTACK YOU WANNA DO HOMIE: ")
         
 def choice_one():
-    if (main.menu_status == "home"):
+    if (render.menu_status == "home"):
         shaq_choice = "Free Throw"
         speedcheck()
         hpcheck()
 def choice_two():
-    if (main.menu_status == "home"):
+    if (render.menu_status == "home"):
         shaq_choice = "Drink Up"
         speedcheck()
         hpcheck()
 def choice_three():
-    if (main.menu_status == "home"):
+    if (render.menu_status == "home"):
         shaq_choice = "Shmoney Dance"
         speedcheck()
         hpcheck()
 def choice_four():
-    if (main.menu_status == "home"):
+    if (render.menu_status == "home"):
         shaq_choice = "Final Dunk"
         speedcheck()
         hpcheck()
@@ -44,25 +44,25 @@ def shaqattack():
     tempshaq = shaq_attacks[shaq_choice]
     crit = random.randint(0,100)
     if tempshaq == "Free Throw":
-        main.type_fight("You shot a free throw!")
-        main.type_fight("Beet took", shaq_power, "damage!")
+        render.type_fight("You shot a free throw!")
+        render.type_fight("Beet took", shaq_power, "damage!")
         beet_hp = beet_hp - shaq_power
     elif tempshaq == "Drink Up":
-        main.type_fight("You drank dat gatorade!")
+        render.type_fight("You drank dat gatorade!")
         if (shaq_hp == 200):
-            main.type_fight("But it failed!")
+            render.type_fight("But it failed!")
         elif (shaq_hp + 50) > 200:
-            main.type_fight("You restored", 200 - shaq_hp, "hp!")
+            render.type_fight("You restored", 200 - shaq_hp, "hp!")
             shaq_hp = 200
         elif (shaq_hp + 50) <= 200:
-            main.type_fight("You restored 50 hp!")
+            render.type_fight("You restored 50 hp!")
             shaq_hp = shaq_hp + 50
     elif tempshaq == "Shmoney Dance":
-        main.type_fight("Shaq's speed doubled from the shmoney dance!")
+        render.type_fight("Shaq's speed doubled from the shmoney dance!")
         shaq_speed = 2 * shaq_speed
     elif tempshaq == "Final Dunk":
-        main.type_fight("Shaq leaps into the air!")
-        main.type_fight("Shaq slam dunked on beet!")
+        render.type_fight("Shaq leaps into the air!")
+        render.type_fight("Shaq slam dunked on beet!")
         beet_hp = 0
     if crit == 87:
         shaq_power /= 2
@@ -70,10 +70,10 @@ def shaqattack():
 def hpcheck():
     global shaq_hp, beet_hp
     if shaq_hp <= 0:
-        main.type_fight("Shaq fainted!")
+        render.type_fight("Shaq fainted!")
         beet_hp = 0
     elif beet_hp <= 0:
-        main.type_fight("Beet fainted!")
+        render.type_fight("Beet fainted!")
 
 def beetattack():
     global shaq_attacks, shaq_hp, shaq_speed, shaq_power, beet_hp, beet_attacks, beet_speed, beet_power
@@ -87,19 +87,19 @@ def beetattack():
         beet_power *= 2
     j = beet_attacks[i_beet]
     if j == "Heal":
-        main.type_fight("Beet restored a cuppa hp")
+        render.type_fight("Beet restored a cuppa hp")
         beet_hp = beet_hp + 50
     elif j == "Nae Nae":
-        main.type_fight("Beet nae nae'd on you!")
-        main.type_fight("Your speed dropped by 20!")
+        render.type_fight("Beet nae nae'd on you!")
+        render.type_fight("Your speed dropped by 20!")
         shaq_speed = shaq_speed - 20
     elif j == "Whip":
-        main.type_fight("Shaq got whipped!")
-        main.type_fight("Shaq lost", beet_power, "hp!")
+        render.type_fight("Shaq got whipped!")
+        render.type_fight("Shaq lost", beet_power, "hp!")
         shaq_hp = shaq_hp - beet_power
     elif j == "Flex":
-        main.type_fight("Beet flexed!")
-        main.type_fight("Beet's attacks do twice as much damage!")
+        render.type_fight("Beet flexed!")
+        render.type_fight("Beet's attacks do twice as much damage!")
         beet_power = 2 * beet_power
     if crit == 13:
         beet_power /= 2
