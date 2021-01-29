abe58fa1581fd545856ed59708727aa7f6f5a5ff
Cade Horn <cadedhorn@gmail.com>
2021-01-29 15:22:12 -0700

    jhasdff

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from playsound import playsound
 statefarm = jake.Screen()
 from attacks import *
 setenemyfile('ogre.txt')
-setplayerfile('wizardogy.txt')
+setplayerfile('aaberg.txt')
 playsound('battle.mp3',block=False);
 battle = True
 while battle == True:
     playerchoose()
+type_fight("lol funny fart :)))")
 statefarm.mainloop()
